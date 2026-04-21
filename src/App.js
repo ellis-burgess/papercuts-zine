27db005f32da44548ade9d69ddf39c38015d7e11
ellis-burgess <ellismburgess@gmail.com>
2026-04-21 17:12:07 +0100

    Restructure code to support routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
-import BloodyHand from './img/homepage_bloody_hand.png';
-
-function Hero() {
-  return (
-    <section className="hero">
-      <h1 class="hero-title">Welcome to<br/><span class="title">Paper<br/>cuts</span></h1>
-      <img src={BloodyHand} alt="Black and white hand dripping with blood" className="hero-image" />
-    </section>
-  );
-}
-
-function InfoSection() {
-  return (
-    <section className="info">
-      <h2 class="info-title">We want to unnerve you.</h2>
-      <a href="/latest"><button class="info-button">Read now</button></a>
-    </section>
-  )
-}
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import LandingPage from './landing-page';
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-      <InfoSection />
-    </div>
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>    </BrowserRouter>
   );
 }
 
---
+++ src/landing-page.js
@@ -0,0 +1,30 @@
+import BloodyHand from './img/homepage_bloody_hand.png';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1 class="hero-title">Welcome to<br/><span class="title">Paper<br/>cuts</span></h1>
+      <img src={BloodyHand} alt="Black and white hand dripping with blood" className="hero-image" />
+    </section>
+  );
+}
+
+function InfoSection() {
+  return (
+    <section className="info">
+      <h2 class="info-title">We want to unnerve you.</h2>
+      <a href="/latest"><button class="info-button">Read now</button></a>
+    </section>
+  )
+}
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <Hero />
+      <InfoSection />
+    </div>
+  )
+}
+
+export default LandingPage;
