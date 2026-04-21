dde2157e982e3651a305db0f4c776854aa66424b
ellis-burgess <ellismburgess@gmail.com>
2026-04-21 16:54:43 +0100

    Set up landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import BloodyHand from './img/homepage_bloody_hand.png';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1 class="hero-title">Welcome to<br/><span class="title">Paper<br/>cuts</span></h1>
+      <img src={BloodyHand} alt="Black and white hand dripping with blood" className="hero-image" />
+    </section>
+  );
+}
+
+function InfoSection() {
+  return (
+    <section className="info">
+      <h2 class="info-title">We want to unnerve you.</h2>
+      <a href="/latest"><button class="info-button">Read now</button></a>
+    </section>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <InfoSection />
     </div>
   );
 }
